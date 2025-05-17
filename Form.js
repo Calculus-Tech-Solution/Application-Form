11a37148155d4d4cf18699f4a8f918ea8e89c9fb
Calculus-Tech-Solutions <olajidesamson62@gmail.com>
2025-05-17 09:23:14 +0100

    Update Form.js

--- Form.js
+++ Form.js
@@ -2,9 +2,6 @@ document.addEventListener(`DOMContentLoaded`, function () {
   const unHide = document.querySelector(`.yes`);
   const Hide = document.querySelector(`.no`);
   const label = document.querySelector(`.label`);
-  const form = this.documentElement.querySelector(`#Signup-Form`);
-  const dateInput = document.querySelector(`#bob`);
-  const errorMessage = document.querySelector(`error-message`);
 
   Hide.addEventListener(`click`, function (e) {
     e.preventDefault();
@@ -17,21 +14,6 @@ document.addEventListener(`DOMContentLoaded`, function () {
     unHide.checkVisibility;
   });
 
-  form.addEventListener(`submit`, function (event) {
-    const selectedDate = new Date(dateInput);
-    const minDate = new Date(`1980-01-01`);
-    const maxDate = new Date(`2015-12-31`);
-
-    if (!dateInput.value) {
-      errorMessage.textContent = `Please select a date.`;
-      event.preventDefault();
-    } else if (selectedDate < minDate || selectedDate > maxDate) {
-      errorMessage.textContent = `Applicant must be 10years or Older`;
-      event.preventDefault();
-    } else {
-      errorMessage.textContent = ``;
-    }
-  });
 });
 function lettersOnly(input) {
   var regex = /[^a-z]/gi;
