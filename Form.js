ee8efc5ba2b0db1510f30ef930ff3818d0889802
Calculus-Tech-Solutions <olajidesamson62@gmail.com>
2025-05-17 09:17:02 +0100

    Update Form.js

--- Form.js
+++ Form.js
@@ -1,26 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const userForm = document.getElementById("#user");
-  const dateInput = document.getElementById("#date");
-  const errorMessage = document.getElementById("#error-message");
+document.addEventListener(`DOMContentLoaded`, function () {
+  const unHide = document.querySelector(`.yes`);
+  const Hide = document.querySelector(`.no`);
+  const label = document.querySelector(`.label`);
+  const form = this.documentElement.querySelector(`#Signup-Form`);
+  const dateInput = document.querySelector(`#bob`);
+  const errorMessage = document.querySelector(`error-message`);
 
-  userForm.addEventListener("submit", function (event) {
-    const selectedDate = new Date(dateInput.value);
-    const minDate = new Date("1980-01-01");
-    const maxDate = new Date("2015- 12-31");
+  Hide.addEventListener(`click`, function (e) {
+    e.preventDefault();
+    label.classList.add("hidden");
+  });
+
+  unHide.addEventListener(`click`, function (e) {
+    e.preventDefault();
+    label.classList.remove("hidden");
+    unHide.checkVisibility;
+  });
+
+  form.addEventListener(`submit`, function (event) {
+    const selectedDate = new Date(dateInput);
+    const minDate = new Date(`1980-01-01`);
+    const maxDate = new Date(`2015-12-31`);
 
     if (!dateInput.value) {
-      errorMessage.textContent = "Please select date.";
+      errorMessage.textContent = `Please select a date.`;
       event.preventDefault();
     } else if (selectedDate < minDate || selectedDate > maxDate) {
-      errorMessage.textContent = "You must be at least 15years";
+      errorMessage.textContent = `Applicant must be 10years or Older`;
       event.preventDefault();
     } else {
-      errorMessage.textContent = "";
+      errorMessage.textContent = ``;
     }
   });
 });
-
 function lettersOnly(input) {
   var regex = /[^a-z]/gi;
+  input.value = input.value.replace(regex, "");
+}
+function numbersOnly(input) {
+  var regex = /[^0-9]/gi;
   input.value = input.value.replace(regex, "");
 }
